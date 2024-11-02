7516e15b08a64eda00e4140814063317b86c0996
TimmyRich <gotim02@gmail.com>
2024-11-02 23:38:53 +1000

    added QR Code Scanner

--- app/(tabs)/qr_code_scanner/[id].jsx
+++ app/(tabs)/qr_code_scanner/[id].jsx
@@ -1,10 +1,89 @@
-import { View, Text, Button } from 'react-native'
-import React from 'react'
 
-export default function QRCodeScanner() {
+// Import necessary modules from React, React Native, and Expo
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+
+export default function QrCodeScanner() {
+  const [scanned, setScanned] = useState(false);
+  const [scannedData, setScannedData] = useState('');
+  const [permission, requestPermission] = useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View style={styles.container}><Text>Requesting permissions...</Text></View>;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="Grant permission" />
+      </View>
+    );
+  }
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setScannedData(data);
+  };
+
   return (
-    <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-      <Text style={{fontSize:18}}>QR Code Scanner Goes Here:</Text>
+    <View style={styles.container}>
+      <CameraView 
+        style={styles.camera} 
+        type='front'
+        onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}
+      >
+      </CameraView>
+      {scanned && (
+        <View style={styles.scanResultContainer}>
+          <Button title="Tap to Scan Again" onPress={() => setScanned(false)} />
+        </View>
+      )}
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  message: {
+    textAlign: 'center',
+    paddingBottom: 10,
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  scanResultContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'white',
+    padding: 15,
+  },
+  scanResultText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
