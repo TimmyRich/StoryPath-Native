e3077a9e7b7b9465dc9415c4e574eb6ad5233ec8
TimmyRich <gotim02@gmail.com>
2024-11-02 23:39:49 +1000

    Moved project state to layout component and made it the context provider for child components.

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,37 +1,46 @@
 import { View, Text, Button } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect, createContext } from 'react'
 import { Tabs, router, useLocalSearchParams } from 'expo-router'
 import { Feather } from '@expo/vector-icons';
 import { DrawerToggleButton } from '@react-navigation/drawer';
+
+// provide project as context
+export const ProjectContext = createContext()
 
 export default function _layout() {
 
   // ID for selected project
   const {id} = useLocalSearchParams()
 
+  // Store project in state
+  const [project, setProject] = useState()
+
   return (
-   <Tabs screenOptions={{headerLeft: () => <DrawerToggleButton tintColor='#000' />}}>
-    <Tabs.Screen name='project_home' options={{
-      tabBarIcon: ({color}) => (
-        <Feather name="list" size={24} color={color} />
-      ),
-      tabBarLabel: 'Project Home',
-      headerTitle: 'Project Home'
-    }} />
-    <Tabs.Screen name='map' options={{
-      tabBarIcon: ({color}) => (
-        <Feather name="list" size={24} color={color} />
-      ),
-      tabBarLabel: 'Map',
-      headerTitle: 'Map'
-    }} />
-    <Tabs.Screen name='qr_code_scanner' options={{
-      tabBarIcon: ({color}) => (
-        <Feather name="list" size={24} color={color} />
-      ),
-      tabBarLabel: 'QR Code Scanner',
-      headerTitle: 'QR Code Scanner'
-    }} />
-   </Tabs>
+    <ProjectContext.Provider value={{project, setProject}}>
+      <Tabs screenOptions={{headerLeft: () => <DrawerToggleButton tintColor='#000' />}}>
+        <Tabs.Screen name='project_home/[id]' options={{
+          tabBarIcon: ({color}) => (
+            <Feather name="list" size={24} color={color} />
+          ),
+          tabBarLabel: 'Project Home',
+          headerTitle: 'Project Home'
+        }} />
+        <Tabs.Screen name='map/[id]' options={{
+          tabBarIcon: ({color}) => (
+            <Feather name="list" size={24} color={color} />
+          ),
+          tabBarLabel: 'Map',
+          headerTitle: 'Map'
+        }} />
+        <Tabs.Screen name='qr_code_scanner/[id]' options={{
+          tabBarIcon: ({color}) => (
+            <Feather name="list" size={24} color={color} />
+          ),
+          tabBarLabel: 'QR Code Scanner',
+          headerTitle: 'QR Code Scanner'
+        }} />
+      </Tabs>
+    </ProjectContext.Provider>
+   
   )
 }
